37239d089f072a5cf4c3f1d946c1984e04c3252d
raphael <racettour@gmail.com>
2023-02-08 14:44:52 +0100

    ex1

---
+++ fonction.py
@@ -0,0 +1,11 @@
+
+
+def fonction(par1, par2):
+    """
+    encore un ex
+
+    :param par1: par 1
+    :param par2: par 2
+
+    :return: None
+    """
---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    """
+    ceci est un exemple 1
+    :param name: nom
+
+    :return: non
+    """
+
+
+def print2(nom2):
+  """
+  ces 2 une seconde fonction
+
+  :param nom2: nom 2
+  
+  :return:
+  """
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
